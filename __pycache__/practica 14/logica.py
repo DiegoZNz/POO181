7ff59ac2f2ecd0564f1b62190d473af9fca23387
DiegoZNz <121038369@upq.edu.mx>
2023-03-13 04:43:52 -0600

    Actualizacion de los constructores
    (Practica 14 funcional)

--- __pycache__/practica 14/logica.py
+++ __pycache__/practica 14/logica.py
@@ -1,38 +1,65 @@
 from tkinter import messagebox
-import random
-import string
 
 class diego:
+    cuentas = {}
     def __init__(self, NCuenta,tit,ed,sal):
         self.__numeroC=NCuenta
         self.__titular=tit
         self.__edad=ed
         self.__saldo=sal
-    
-    def consultarSaldo(self, NC):
-        if NC==self.__numeroC:
-            saldo=self.__saldo
-            messagebox.showinfo("Consultar saldo",saldo)
-            
-    def ingresarEfectivo(self,NC,SaldoTot,Cantidad):
-        if NC==self.__numeroC:
-            SaldoTot=self.__saldo+Cantidad
-            messagebox.showinfo("Consultar saldo",SaldoTot)
-            return self.__saldo+SaldoTot
-    
-    def Retirarefectivo(self,NC,SaldoTot,Cantidad):
-        if NC==self.__numeroC:
-            SaldoTot=self.__saldo
-            SaldoTot=SaldoTot-Cantidad
-            messagebox.showinfo("Consultar saldo",SaldoTot)
-            
-            
-    def Depositar(self,NC,SaldoTot,Cantidad,NCD):
-        if NC==self.__numeroC:
-            SaldoTot=SaldoTot-Cantidad
-            messagebox.showinfo("Consultar saldo",SaldoTot)
-            
         
-            
-            
-            
+    def CrearCuentas(self,NC,Name,edd,sald):
+        if NC==0 or Name=='' or edd==0:
+            messagebox.showerror("Algo salió mal","Llene todos los campos")
+        elif NC in self.cuentas:
+            messagebox.showerror("Algo salió mal","Ya existe la cuenta")
+        else:
+            self.cuentas[NC] = {"titular": Name, "edad": edd, "saldo": sald}
+            messagebox.showinfo("Bien","La cuenta se agrego correctamente")
+    
+    def consultar_saldo(self,NC):
+        if NC in self.cuentas:
+            messagebox.showinfo("Bien","El saldo de la cuenta {} es: ${}".format(NC, self.cuentas[NC]["saldo"]))
+        else:
+            messagebox.showerror("error","La cuenta {} no existe.".format(NC))
+    
+    
+    def ingresar_efectivo(self, NC, cantidad):
+        if NC in self.cuentas:
+            self.cuentas[NC]["saldo"] += cantidad
+            self.consultar_saldo(NC)
+        else:
+            messagebox.showerror("error","La cuenta {} no existe.".format(NC))
+    
+    def retirar_efectivo(self, NC, cantidad):
+        if NC in self.cuentas:
+            if self.cuentas[NC]["saldo"]>= cantidad:
+                self.cuentas[NC]["saldo"] -= cantidad
+                self.consultar_saldo(NC)
+            elif self.cuentas[NC]["saldo"]==0:
+                messagebox.showerror("Error saldo","Su saldo es de 0")
+            else:
+                messagebox.showerror("Error saldo","Saldo insuficiente")
+        else:
+            messagebox.showerror("error","La cuenta {} no existe.".format(NC))
+    
+    def depositar(self,NC,NCD,cantidad):
+        if NC in self.cuentas:
+            if NCD in self.cuentas:
+                if self.cuentas[NC]["saldo"]>= cantidad:
+                    self.cuentas[NC]["saldo"] -= cantidad
+                    self.cuentas[NCD]["saldo"]+= cantidad
+                    messagebox.showinfo("Exito","Se ha depositado ${} de la cuenta {} a la cuenta {}.".format(cantidad, NC, NCD))
+                    self.consultar_saldo(NC)
+                    self.consultar_saldo(NCD)
+                else:
+                    messagebox.showerror("Error saldo","Saldo insuficiente")
+            else:
+                messagebox.showerror("error","La cuenta {} no existe.".format(NC))
+        else:
+            messagebox.showerror("error","Su cuenta {} no existe.".format(NC))
+        
+
+              
+       
+
--- __pycache__/practica 14/ventana.py
+++ __pycache__/practica 14/ventana.py
@@ -1,18 +1,17 @@
-import tkinter as tk
+import tkinter as tk 
 from logica import *
 
-class Ventana(tk.Tk):
+class Win(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.title("Practica 14: Administración")
-        self.geometry("800x400")
-
-        #Etiqueta de No. Cuenta
+        self.geometry("500x700")
         self.lblNCuenta = tk.Label(text="No. Cuenta:")
         self.lblNCuenta.pack()
     
         #Entry de No. Cuenta
-        self.entryNCuenta = tk.Entry(self)
+        self.entryNCuenta = tk.Entry(self,justify='center')
+        self.entryNCuenta.insert(0, "0")
         self.entryNCuenta.pack()
         
         #Etiqueta de titular
@@ -20,7 +19,7 @@ class Ventana(tk.Tk):
         self.lbltitular.pack()
     
         #Entry de No. Cuenta
-        self.entrytitular = tk.Entry(self)
+        self.entrytitular = tk.Entry(self,justify='center')
         self.entrytitular.pack()
         
         #Etiqueta de edad
@@ -28,7 +27,8 @@ class Ventana(tk.Tk):
         self.lbledad.pack()
     
         #Entry de No. Cuenta
-        self.entryedad = tk.Entry(self)
+        self.entryedad = tk.Entry(self,justify='center')
+        self.entryedad.insert(0, "0")
         self.entryedad.pack()
         
         #Etiqueta de saldo
@@ -36,79 +36,101 @@ class Ventana(tk.Tk):
         self.lblsaldo.pack()
     
         #Entry de saldo
-        self.entrysaldo = tk.Entry(self)
+        self.entrysaldo = tk.Entry(self,justify='center')
+        self.entrysaldo.insert(0, "0")
         self.entrysaldo.pack()
 
         #Boton
-        self.buttonCS = tk.Button(self, text="Consultar saldo", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.on_button)
+        self.buttonCS = tk.Button(self, text="Agregar Clientes", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.on_button)
         self.buttonCS.pack()
         
-        #Etiqueta de efectivo
-        self.lblefectivo = tk.Label(text="Monto a ingresar:")
-        self.lblefectivo.pack()
-    
-        #Entry de saldo
-        self.entryefectivo = tk.Entry(self)
-        self.entryefectivo.insert(0, "0")
-        self.entryefectivo.pack()
-        
-        self.buttonIE = tk.Button(self, text="Ingresar efectivo", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.ingresarEf)
-        self.buttonIE.pack()
-        
-        
-        #Etiqueta de efectivo
-        self.lblefectivor = tk.Label(text="Monto a retirar:")
-        self.lblefectivor.pack()
-    
-        #Entry de saldo
-        self.entryefectivor = tk.Entry(self)
+        #Entry de NC
+        self.lblNC = tk.Label(text="Numero de cuenta:")
+        self.lblNC.pack()
+        self.entryefectivor = tk.Entry(self,justify='center')
         self.entryefectivor.insert(0, "0")
         self.entryefectivor.pack()
         
-        self.buttonre = tk.Button(self, text="retirar efectivo", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.retirarEf)
-        self.buttonre.pack()
+        self.buttonCK = tk.Button(self, text="Verificar Saldo", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.cheq)
+        self.buttonCK.pack()
         
-        #Etiqueta de cuenta
-        self.lblefectivodep = tk.Label(text="Monto a retirar:")
-        self.lblefectivodep.pack()
-    
-        #Entry de saldo
-        self.entryefectivodep = tk.Entry(self)
-        self.entryefectivodep.insert(0, "0")
-        self.entryefectivodep.pack()
+        #*******************************************Ingresar********************************
+        self.lblNC2 = tk.Label(text="Numero de cuenta:")
+        self.lblNC2.pack()
+        self.entryNC22 = tk.Entry(self,justify='center')
+        self.entryNC22.insert(0, "0")
+        self.entryNC22.pack()
+        self.lblmonto = tk.Label(text="Monto a depositar:")
+        self.lblmonto.pack()
+        self.entrymonto = tk.Entry(self,justify='center')
+        self.entrymonto.insert(0, "0")
+        self.entrymonto.pack()
         
-        self.lblNCD = tk.Label(text="Monto a retirar:")
-        self.lblNCD.pack()
-    
-        #Entry de saldo
-        self.entryNCD = tk.Entry(self)
-        self.entryNCD.insert(0, "0")
-        self.entryNCD.pack()
+        self.buttonCKc = tk.Button(self, text="Ingresar efectivo", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.cash)
+        self.buttonCKc.pack()
         
-        self.buttonre = tk.Button(self, text="Depositar efectivo", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.depositar)
-        self.buttonre.pack()
+        #*******************************************Retirar********************************
+        self.lblNC3 = tk.Label(text="Numero de cuenta:")
+        self.lblNC3.pack()
+        self.entryNC3 = tk.Entry(self,justify='center')
+        self.entryNC3.insert(0, "0")
+        self.entryNC3.pack()
+        self.lblmonto2 = tk.Label(text="Monto a retirar:")
+        self.lblmonto2.pack()
+        self.entrymonto2 = tk.Entry(self,justify='center')
+        self.entrymonto2.insert(0, "0")
+        self.entrymonto2.pack()
         
+        self.buttonCKr = tk.Button(self, text="Retirar efectivo", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.ret)
+        self.buttonCKr.pack()
+        #*******************************************Depositar********************************
+        self.lblNC4 = tk.Label(text="Tú número de cuenta:")
+        self.lblNC4.pack()
+        self.entryNC4 = tk.Entry(self,justify='center')
+        self.entryNC4.insert(0, "0")
+        self.entryNC4.pack()
+        self.lblNC5 = tk.Label(text="Numero de cuenta a depositar:")
+        self.lblNC5.pack()
+        self.entryNC5 = tk.Entry(self,justify='center')
+        self.entryNC5.insert(0, "0")
+        self.entryNC5.pack()
+        self.lblmonto3 = tk.Label(text="Monto a retirar:")
+        self.lblmonto3.pack()
+        self.entrymonto3 = tk.Entry(self,justify='center')
+        self.entrymonto3.insert(0, "0")
+        self.entrymonto3.pack()
         
-    #Función para el botón
+        self.buttonCKd = tk.Button(self, text="Depositar", bg="#008080", fg="#FFFFFF",font=("Helvetica", 15), command=self.dep)
+        self.buttonCKd.pack()
+        
+    #apartado de Funciones de botones    
     def on_button(self):
-        #se crea el objeto con los gets de los entry's
-        Cuenta=diego(int(self.entryNCuenta.get()),self.entrytitular.get(),int(self.entryedad.get()),float(self.entrysaldo.get()))
-        Cuenta.consultarSaldo(int(self.entryNCuenta.get()))
-        #gen=diego(self.entryLen.get(),self.estado.get(),self.estado1.get())
-        #self.entryCopy.delete(0, tk.END)
-        #self.entryCopy.insert(0,gen.generarContraseña(int(self.entryLen.get()),self.estado.get(),self.estado1.get()))
-    def ingresarEf(self):
-        Ingresar=diego(int(self.entryNCuenta.get()),self.entrytitular.get(),int(self.entryedad.get()),float(self.entrysaldo.get()))
-        Ingresar.ingresarEfectivo(int(self.entryNCuenta.get()),float(self.entrysaldo.get()),float(self.entryefectivo.get()))
+        CSTR=diego(int(self.entryNCuenta.get()),self.entrytitular.get(),int(self.entryedad.get()),float(self.entrysaldo.get()))
+        CSTR.CrearCuentas(int(self.entryNCuenta.get()),self.entrytitular.get(),int(self.entryedad.get()),float(self.entrysaldo.get()))
+        self.entryNCuenta.delete(0, tk.END)
+        self.entryNCuenta.insert(0, "0")
+        self.entrytitular.delete(0, tk.END)
+        self.entryedad.delete(0, tk.END)
+        self.entryedad.insert(0, "0")
+        self.entrysaldo.delete(0, tk.END)
+        self.entrysaldo.insert(0, "0")
         
-    def retirarEf(self):
-        ret=diego(int(self.entryNCuenta.get()),self.entrytitular.get(),int(self.entryedad.get()),float(self.entrysaldo.get()))
-        ret.Retirarefectivo(int(self.entryNCuenta.get()),float(self.entrysaldo.get()),float(self.entryefectivor.get()))
+    def cheq(self):
+        CSTR=diego(any,any,any,any)
+        CSTR.consultar_saldo(int(self.entryefectivor.get()))
     
-    
-    def depositar(self):
-        dep=diego(int(self.entryNCuenta.get()),self.entrytitular.get(),int(self.entryedad.get()),float(self.entrysaldo.get()))
-        dep.Depositar(int(self.entryNCuenta.get()),float(self.entrysaldo.get()),float(self.entryefectivor.get()),int(self.entryNCD.get()))     
-          
-ventana = Ventana()
+    def cash(self):
+        CSTR=diego(any,any,any,any)
+        CSTR.ingresar_efectivo(int(self.entryNC22.get()),float(self.entrymonto.get()))
+        
+    def ret(self):
+        CSTR=diego(any,any,any,any)
+        CSTR.retirar_efectivo(int(self.entryNC3.get()),float(self.entrymonto2.get()))
+        
+    def dep(self):
+        CSTR=diego(any,any,any,any)
+        CSTR.depositar(int(self.entryNC4.get()),int(self.entryNC5.get()),float(self.entrymonto3.get()))
+        
+        
+ventana = Win()
 ventana.mainloop()
